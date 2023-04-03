2e096470e036e855a9c4cf22b53a7dd92f9172a7
Austin Serif <austin@sans-serif.io>
2023-04-02 18:13:11 -0700

    add a template test file

---
+++ tests/test_synthetic_combinations.py
@@ -0,0 +1,85 @@
+# import synthetic_combinations
+
+def test_horizontal_fit():
+    expected_result = 1
+
+    # SETUP THE TEST VALUES...
+    # max_rank = None
+    # spectral_t = None
+    # synth_combo = synthetic_combinations.Synthetic_Combinations(max_rank, spectral_t)
+
+    # RUN THE FUNCTION WITH TEST VALUES
+    # actual_ result = synth_combo.horizontal_fit(...args)
+
+    # ASSERT EXPECTED vs ACTUAL VALUES
+    assert expected_result == 1
+
+def test_vertical_fit():
+    expected_result = 1
+
+    # SETUP THE TEST VALUES...
+    # max_rank = None
+    # spectral_t = None
+    # synth_combo = synthetic_combinations.Synthetic_Combinations(max_rank, spectral_t)
+
+    # RUN THE FUNCTION WITH TEST VALUES
+    # actual_ result = synth_combo.vertical_fit(...args)
+
+    # ASSERT EXPECTED vs ACTUAL VALUES
+    assert expected_result == 1
+     
+def test_pcr():
+    expected_result = 1
+
+    # SETUP THE TEST VALUES...
+    # max_rank = None
+    # spectral_t = None
+    # synth_combo = synthetic_combinations.Synthetic_Combinations(max_rank, spectral_t)
+
+    # RUN THE FUNCTION WITH TEST VALUES
+    # actual_ result = synth_combo._pcr(...args)
+
+    # ASSERT EXPECTED vs ACTUAL VALUES
+    assert expected_result == 1
+      
+def test_pcr_cv():
+    expected_result = 1
+
+    # SETUP THE TEST VALUES...
+    # max_rank = None
+    # spectral_t = None
+    # synth_combo = synthetic_combinations.Synthetic_Combinations(max_rank, spectral_t)
+
+    # RUN THE FUNCTION WITH TEST VALUES
+    # actual_ result = synth_combo._pcr_cv(...args)
+
+    # ASSERT EXPECTED vs ACTUAL VALUES
+    assert expected_result == 1
+    
+def test_spectral_rank():
+    expected_result = 1
+
+    # SETUP THE TEST VALUES...
+    # max_rank = None
+    # spectral_t = None
+    # synth_combo = synthetic_combinations.Synthetic_Combinations(max_rank, spectral_t)
+
+    # RUN THE FUNCTION WITH TEST VALUES
+    # actual_ result = synth_combo.spectral_rank(...args)
+
+    # ASSERT EXPECTED vs ACTUAL VALUES
+    assert expected_result == 1
+
+def test_universal_rank(): 
+    expected_result = 1
+
+    # SETUP THE TEST VALUES...
+    # max_rank = None
+    # spectral_t = None
+    # synth_combo = synthetic_combinations.Synthetic_Combinations(max_rank, spectral_t)
+
+    # RUN THE FUNCTION WITH TEST VALUES
+    # actual_ result = synth_combo.universal_rank(...args)
+
+    # ASSERT EXPECTED vs ACTUAL VALUES
+    assert expected_result == 1
